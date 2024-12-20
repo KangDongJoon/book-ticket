ed53a5574727caf72a1f9a6e357bba5950415af7
KangDongJoon <ehdwns1119@naver.com>
2024-12-20 16:46:35 +0900

    [FEAT] Auth 회원가입/로그인 구현

--- src/main/java/com/play/bookticket/domain/user/entity/User.java
+++ src/main/java/com/play/bookticket/domain/user/entity/User.java
@@ -2,34 +2,21 @@ package com.play.bookticket.domain.user.entity;
 
 import com.play.bookticket.common.entity.TimeStamped;
 import com.play.bookticket.domain.user.enums.UserRole;
-import jakarta.persistence.*;
-import lombok.Getter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
-@Table(name = "users")
-@Getter
 public class User extends TimeStamped {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(unique = true)
     private String email;
-
-    private String password;
-
-    @Enumerated(EnumType.STRING)
-    private UserRole role;
-
     private String nickname;
-
-    public User(String email, String password, UserRole role, String nickname) {
-        this.email = email;
-        this.password = password;
-        this.role = role;
-        this.nickname = nickname;
-    }
+    private String password;
+    private UserRole role;
 }
